76a65c76cd9767834a5053540ef490a1da35d963
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-07 10:24:46 +0300

    Add controller for routes

---
+++ controllers/bootcamps.js
@@ -0,0 +1,40 @@
+// @desc    Get all bootcamps
+// @router  GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
+
+// @desc    Get single bootcamps
+// @router  GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
+
+// @desc    Create new bootcamp
+// @router  POST /api/v1/bootcamps/
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+};
+
+// @desc    Update new bootcamp
+// @router  PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc    Delete bootcamp
+// @router  DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
