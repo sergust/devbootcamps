baa853746cdb849192b1be0270ddb485f559a269
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-07 17:29:55 +0300

    Add filtering by query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,15 @@ const ErrorResponse = require('../utils/errorResponse');
 // @router  GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
 
   res
     .status(200)
