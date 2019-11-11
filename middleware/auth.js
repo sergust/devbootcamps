46793d837314710a337bd53e9805a02b0b52361f
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-11 10:21:01 +0300

    Implement role authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -34,3 +34,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...role) => {
+  return (req, res, next) => {
+    if (!role.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
